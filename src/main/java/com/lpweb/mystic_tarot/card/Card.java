08f8172032fdd5b067fe0cf3ca06febd27b76704
Aurélien <robineau.aure@gmail.com>
2020-12-01 14:51:16 +0100

    Add class Card

---
+++ src/main/java/com/lpweb/mystic_tarot/card/Card.java
@@ -0,0 +1,30 @@
+package com.lpweb.mystic_tarot.card;
+
+/**
+ * Card of the tarot game.
+ */
+public class Card 
+{
+    private Integer number;
+    private String description;
+
+    public Card(Integer number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public Integer getNumber() {
+        return this.number;
+    };
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return
+         this.getClass().getSimpleName() + " number " + this.number + ":\n" +
+         this.description;
+    }
+}
